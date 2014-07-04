aa094a68fb93f8a3eb6d655b7a548f0e580a3b9c
Roobal Jindal <roobaljindal9@gmail.com>
2014-07-04 14:06:51 +0530

    changes

--- pythonsite/settings.py
+++ pythonsite/settings.py
@@ -98,7 +98,7 @@ STATIC_URL = '/static/'
 
 
 import dj_database_url
-
+DATABASES={}
 DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
