c687bf989139eee7282c1095a1316bfcf16c712a
Roobal Jindal <roobaljindal9@gmail.com>
2014-07-18 12:49:00 +0530

    j

--- shaker/views.py
+++ shaker/views.py
@@ -5,7 +5,7 @@ from shaker.models import User
 # Create your views here.
 def home(request):
     my=""
-    string= User.objects.all().order_by('highscore')[:]
+    string= User.objects.all().order_by('highscore')[:20]
     for i in string:
         my=my+str(i)+"<br><br><br>"
     return HttpResponse(my)
