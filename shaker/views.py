92d77e4e8b35ffdacde6a79969748fe52d423b1c
Roobal Jindal <roobaljindal9@gmail.com>
2014-07-18 12:36:38 +0530

    j

--- shaker/views.py
+++ shaker/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import HttpResponse
+from shaker.models import User
 # Create your views here.
 def home(request):
     my=""
