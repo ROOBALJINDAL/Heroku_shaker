d2ff11d91d26fbc310400225642853215e47fde0
Roobal Jindal <roobaljindal9@gmail.com>
2014-07-18 12:38:48 +0530

    j

--- shaker/models.py
+++ shaker/models.py
@@ -5,3 +5,6 @@ class User(models.Model):
     username=models.CharField(max_length=20)
     password=models.CharField(max_length=20)
     highscore=models.BigIntegerField()
+    def __str__(self):
+        string="Password: " +self.password + "      username: " + self.username + "     Highscores: " + str(self.highscore)
+        return string
