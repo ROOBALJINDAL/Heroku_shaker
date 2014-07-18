b476569a023b0eb15d4e65d6f9c27a8782fa79f9
Roobal Jindal <roobaljindal9@gmail.com>
2014-07-18 12:47:41 +0530

    j

--- shaker/models.py
+++ shaker/models.py
@@ -6,5 +6,6 @@ class User(models.Model):
     password=models.CharField(max_length=20)
     highscore=models.BigIntegerField()
     def __str__(self):
-        string="<br>USERNAME:  " + self.username + "<br>PASSWORD: " +self.password +"<br>HIGHSCORES: " + str(self.highscore)
+        string="<br>ID: " +self.ID+ <br>USERNAME:  " + self.username + "<br>PASSWORD: " +self.password +"<br>HIGHSCORES: "
+        + str(self.highscore)
         return string
