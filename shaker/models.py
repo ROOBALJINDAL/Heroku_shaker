86b768dd7fc8c46d0ecb6f07bea6844efae463d3
Roobal Jindal <roobaljindal9@gmail.com>
2014-07-18 13:20:09 +0530

    j

--- shaker/models.py
+++ shaker/models.py
@@ -5,13 +5,17 @@ class User(models.Model):
     username=models.CharField(max_length=20)
     password=models.CharField(max_length=20)
     
-    easyscore=models.BigIntegerField(default=20)
-    mediumscore=models.BigIntegerField(default=20)
-    hardscore=models.BigIntegerField(default=20)
+    #easyscore=models.BigIntegerField(default=20)
+    #mediumscore=models.BigIntegerField(default=20)
+    #hardscore=models.BigIntegerField(default=20)
     highscore=models.BigIntegerField()
     
-    def __str__(self):
+    '''def __str__(self):
         string="<br>ID:  " + str(self.id) + "<br>USERNAME:  " + self.username + "<br>PASSWORD: " +self.password
         +"<br>EASY: "+ str(self.easyscore)+"<br>MEDIUM: "+ str(self.mediumscore)
         +"<br>HARD: "+ str(self.hardscore)+"<br>HIGHSCORES: "+ str(self.highscore)
+        return string'''
+    
+    def __str__(self):
+        string="<br>ID:  " + str(self.id) + "<br>USERNAME:  " + self.username + "<br>PASSWORD: " +self.password +"<br>HIGHSCORES: "+ str(self.highscore)
         return string
